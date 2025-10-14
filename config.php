dfc0b39a117075f21c930255d0ca615c65b8f1ac
Firosh <118115666+firoshh@users.noreply.github.com>
2025-10-14 19:20:08 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,21 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "gallery_cafe_db";
+$servername = "db";
+$username = "gallary_user";
+$password = "your_strong_user_password";
+$dbname = "gallary_cafe_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?><?php
+$servername = "db";
+$username = "gallary_user";
+$password = "your_strong_user_password";
+$dbname = "gallary_cafe_db";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
